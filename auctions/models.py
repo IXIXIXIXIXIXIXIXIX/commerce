9ae48bc514735ec18e534340b8367632904f399b
IXIXIXIXIXIXIXIXIX <ix.scott.anderson@gmail.com>
2020-11-12 21:06:51 +0000

    add listings model

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,15 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Category(models.Model):
+	category = models.CharField(max_length=64, related_name="categories")
+
+class Listing(models.Model):
+	lister = models.ForeignKey(User, on_delete=models.CASCADE)
+	category = models.ManyToManyField(Category, blank=True)
+	title = models.CharField(max_length=64, related_name="listings")
+	description = models.TextField()
+	starting_bid = models.DecimalField(max_digits=12, decimal_places=2)
+	img_url = models.URLField(blank=True)
+	
